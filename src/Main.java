a8634ff7004eaa65dd460df7eee7e082d16c842b
Archi903 <artpele@yandex.ru>
2022-12-01 12:18:21 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задание1");
+
+        int age = 10;
+
+        if (age >= 18)
+            System.out.println("Поздравляем с совершеннолетием");
+
+        if (age < 18)
+        System.out.println("Возраст совершеннолетия ещё не наступил, необходимо еще немного подождать");
+
     }
 }
