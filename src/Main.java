5ee0ae185cbdb9e2441e4a84f10308baf6676ce1
Archi903 <artpele@yandex.ru>
2022-12-01 14:15:35 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
         System.out.println("Задание1");
 
-        int age = 10;
+        int age = 17;
 
         if (age >= 18)
             System.out.println("Поздравляем с совершеннолетием");
 
         if (age < 18)
-        System.out.println("Возраст совершеннолетия ещё не наступил, необходимо еще немного подождать");
+            System.out.println("Возраст совершеннолетия ещё не наступил, необходимо еще немного подождать");
+
+    }
+
+    public static void task2() {
+        System.out.println("Задание2");
+
+        int age = 20;
+
+            if (age < 7) {
+                System.out.println("Ребенок может пока отдыхать");
+            }
+            if (age >= 7 && age < 18) {
+                System.out.println("Ребенку пора в школу");
+            }
+            if (age >= 18 && age < 24) {
+                System.out.println("Ученик может поступить в университет");
+            }
+            if (age >= 24) {
+                System.out.println("Поздравляем с окончанием университета, можете искать первую работу");
+            }
 
     }
 }
