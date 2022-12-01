c5bd3346b8448fdd1c014dcd6d5900f357d0d4d3
Archi903 <artpele@yandex.ru>
2022-12-01 16:14:13 +0300

    Домашнее задание 3 Часть 2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
     }
 
     public static void task1() {
@@ -106,15 +107,28 @@ public class Main {
 
         if (age >= 2 && age <= 6) {
             System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в детский сад");
-        }
-        else if (age >= 7 && age <= 18) {
+        } else if (age >= 7 && age <= 18) {
             System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в школу");
-        }
-        else if (age > 18 && age < 24) {
+        } else if (age > 18 && age < 24) {
             System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в университет");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " то ему нужно ходить на работу");
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Домашнее задание 3 Часть 2");
+
+        int age = 13;
+
+        if (age < 5) {
+            System.out.println("Ребенку нельзя кататься на аттракционе");
+        }
+        else if (age >= 5 && age < 14) {
+            System.out.println("Ребенку можно кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
         }
         else {
-            System.out.println("Если возраст человека равен " + age + " то ему нужно ходить на работу");
+            System.out.println("Можно кататься без сопровождения взрослого");
         }
     }
 }
